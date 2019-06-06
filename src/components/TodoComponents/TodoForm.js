b299634cb50308440bb113bf7d81e85d000f7633
rebeccajoyner <49732754+rebeccajoyner@users.noreply.github.com>
2019-06-06 11:04:41 -0400

    along with video guide

---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function TodoForm(props) {
+    return (
+        <form className="form" onSubmit={e => props.addTask(e)}>
+
+            <input
+                type="text"
+                name="taskInput"
+                placeholder="Task Input"
+                value={props.taskInput}
+                onChange={e => props.changeHandler(e)}
+            />
+
+            <button>Submit</button>
+            
+            <button>Clear Completed</button>
+
+        </form>
+    );
+}
+
+export default TodoForm;
